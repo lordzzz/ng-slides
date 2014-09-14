0aecef4a5193a827743ddc27fdec4dbcd3c5fab5
Evgeniy Pakalo <evgeniy.pakalo@gmail.com>
2014-09-15 00:24:02 +0300

    added switch slides

--- js/ng-slides.js
+++ js/ng-slides.js
@@ -2,9 +2,58 @@
    'use strict';
 
     window.angular.module('ngSlides', [])
-        .controller('SlidesCtrl', function () {
+	    .controller('SlidesCtrl', ['$scope', '$window', function ($scope, $window) {
+		    $scope.currentSlide = 0;
+		    $scope.slides = [];
+		    $scope.progress = 0;
 
-        })
+		    $scope.$watch('currentSlide', function (newValue) {
+				$scope.progress = (newValue / $scope.slides.length) * 100;
+		    });
+
+		    $scope.visibilitySlide = function (i, visibility) {
+			    $scope.slides[i].css({
+				    visibility: visibility
+			    });
+		    };
+
+		    $scope.prevSlide = function () {
+			    if ($scope.currentSlide > 1) {
+				    $scope.currentSlide--;
+				    $scope.$apply();
+				    $scope.visibilitySlide($scope.currentSlide, 'hidden');
+				    $scope.visibilitySlide($scope.currentSlide - 1, 'visible');
+			    }
+		    };
+
+		    $scope.nextSlide = function () {
+			    if ($scope.currentSlide < $scope.slides.length) {
+				    $scope.currentSlide++;
+				    $scope.$apply();
+				    if ($scope.currentSlide > 1) {
+					    $scope.visibilitySlide($scope.currentSlide - 2, 'hidden');
+				    }
+				    $scope.visibilitySlide($scope.currentSlide - 1 , 'visible')
+			    }
+		    };
+
+		    angular.element($window).bind('keydown', function(e) {
+			    switch (e.keyCode) {
+				    case 37: {
+					    $scope.prevSlide();
+					    break;
+				    }
+				    case 39: {
+					    $scope.nextSlide();
+					    break;
+				    }
+			    }
+		    });
+
+		    angular.element($window).bind('load', function() {
+			    $scope.nextSlide();
+		    });
+	    }])
         .directive('presentation', function () {
             return {
                 restrict: 'E'
@@ -12,7 +61,11 @@
         })
         .directive('slide', function () {
             return {
-                restrict: 'E'
+                restrict: 'E',
+	            link: function(scope, element) {
+		            element.addClass('slide-hide');
+					scope.slides.push(element);
+	            }
             };
         });
 
