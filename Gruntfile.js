9b456c5775dd8daef2fef72885317fc15c0ceff2
Yauheni Pakala <yauheni_pakala@epam.com>
2014-09-23 15:47:26 +0300

    change grunt config for dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,69 @@
 module.exports = function(grunt) {
+	
+	var _banner = '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n';
+	
+	var _jsFiles = [
+		'bower_components/angular/angular.js',
+		'bower_components/angular-animate/angular-animate.js',
+		'js/ng-slides.js'
+	];
+	
+	var _cssFiles = [
+		'bower_components/normalize-css/normalize.css',
+		'css/styles.css',
+		'css/presentation.css'
+	];
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		watch: {
 			scripts: {
 				files: ['js/*.js'],
-				tasks: ['uglify']
+				tasks: ['concat:dev_js']
 			},
 			styles: {
 				files: ['css/*.css'],
-				tasks: ['cssmin']
+				tasks: ['concat:dev_css']
 			}
 		},
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				banner: _banner
 			},
 			build: {
-				src: [
-					'bower_components/angular/angular.js',
-					'bower_components/angular-animate/angular-animate.js',
-					'js/ng-slides.js'
-				],
+				src: _jsFiles,
 				dest: 'dist/<%= pkg.name %>.min.js'
 			}
 		},
 		cssmin: {
 			build: {
 				options: {
-					banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */'
+					banner: _banner
 				},
 				files: {
-					'dist/styles.min.css': [
-						'bower_components/normalize-css/normalize.css',
-						'css/styles.css',
-						'css/presentation.css'
-					]
+					'dist/styles.min.css': _cssFiles
 				}
 			}
+		},
+		concat: {
+			dev_js: {
+				src: _jsFiles,
+				dest: 'dist/<%= pkg.name %>.min.js',
+			},
+			dev_css: {
+				src: _cssFiles,
+				dest: 'dist/styles.min.css',
+			},
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Default task(s).
 	grunt.registerTask('default', ['watch']);
-	grunt.registerTask('build', ['uglify', 'cssmin']);
+	grunt.registerTask('build', ['uglify:build', 'cssmin:build']);
 
 };
